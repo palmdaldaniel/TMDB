1515fe799275dd7972932c78ad08505f05ef9fae
palmdaldaniel <daniel.palmdal@gmail.com>
2021-09-08 08:38:38 +0200

    Boiler plate added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+      
+    
+    </div>
+  )
+}
+
+export default App
